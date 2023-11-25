520e69c743ac66a252c2f4bf38b7ccb9d28fd4dd
Jean-Claude <jeanyao@ymail.com>
2023-11-25 15:25:08 +0300

    Namespaces

---
+++ php/03-object/09-pdo/www/class/ApiException.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+
+/**
+ * Created by PhpStorm at 25.11.2023
+ *
+ * @ApiException
+ *
+ * @author Jean-Claude <jeanyao@ymail.com>
+ *
+ * @package
+ */
+class ApiException extends Exception
+{
+
+}
---
+++ php/03-object/www/class/HTTPException.php
@@ -0,0 +1,8 @@
+<?php
+namespace Grafikart\Exceptions;
+
+use Exception;
+
+class HTTPException extends Exception {
+    
+}
