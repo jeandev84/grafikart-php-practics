c78a698aaf8740d8431a61e9dc420ddcbd9768db
Jean-Claude <jeanyao@ymail.com>
2023-11-26 06:19:08 +0300

    Tester son code

---
+++ php/www/src/URLHelper.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace App;
+
+
+/**
+ * Created by PhpStorm at 26.11.2023
+ *
+ * @UrlHelper
+ *
+ * @author Jean-Claude <jeanyao@ymail.com>
+ *
+ * @package App\Helper
+ */
+class URLHelper
+{
+     public static function withParams(array $data, array $params): string
+     {
+          /*
+          foreach ($params as $k => $v) {
+               if (is_array($v)) {
+                   $params[$k] = implode(",", $v);
+               }
+          }
+          */
+
+          $params = array_map(function ($v) {
+              return is_array($v) ? implode(",", $v) : $v;
+          }, $params);
+
+          return http_build_query(array_merge($data, $params));
+     }
+
+
+     public static function withParam(array $data, string $param, $value): string
+     {
+         if (is_array($value)) {
+             $value = implode(",", $value);
+         }
+
+         return static::withParams($data, [$param => $value]);
+     }
+
+
+    /*
+    public static function withParamsForTest(array $data, array $params): string
+    {
+        return http_build_query(array_merge($params, $data));
+    }
+    */
+}
