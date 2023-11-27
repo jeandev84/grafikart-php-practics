252e4b6471a934022bb869f40c9f7db676aa3b57
Jean-Claude <jeanyao@ymail.com>
2023-11-27 16:19:02 +0300

    Show article

--- php/www/views/post/index.php
+++ php/www/views/post/index.php
@@ -16,16 +16,7 @@ $posts = $query->fetchAll(PDO::FETCH_CLASS, \App\Entity\Post::class);
 <div class="row">
     <?php foreach ($posts as $post): ?>
     <div class="col-md-3">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title"><?= htmlentities($post->getName()) ?></h5>
-                    <p class="text-muted"><?= $post->getCreatedAt()->format('d F Y') ?></p>
-                    <p><?= $post->getExcerpt() ?></p>
-                    <p>
-                        <a href="<?= $router->url('post', ['slug' => $post->getSlug(), 'id' => $post->getId()]) ?>" class="btn btn-primary">Voir plus</a>
-                    </p>
-                </div>
-            </div>
+        <?php require 'card.php'; ?>
     </div>
     <?php endforeach; ?>
 </div>
