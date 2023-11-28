10b561327037e024cb26a147a9e69ebe2355d893
Jean-Claude <jeanyao@ymail.com>
2023-11-28 04:56:21 +0300

    Create Respository and refactoring code

--- php/www/views/post/index.php
+++ php/www/views/post/index.php
@@ -1,30 +1,4 @@
 <?php
-
-/*
- * ceil: arrondi au nombre superieur
- * floor: arrondi au nombre inferieur
-*/
-
-/*
-dump($categories);
-
-# BAD QUERY
-$sql = "
-SELECT p.*, c.*
-FROM post p
-LEFT JOIN post_category pc ON pc.id = p.id
-LEFT JOIN category c ON c.id = pc.category_id
-ORDER BY created_at DESC
-LIMIT 12";
-
-# OPTIMIZED
-$sql = "
-SELECT c.*, pc.post_id
-FROM post_category pc
-JOIN category c ON c.id = pc.category_id
-WHERE pc.post_id IN (6, 50, 42)";
-*/
-
 $title = 'Mon Blog';
 $connection = \App\Helpers\Connection::make();
 
