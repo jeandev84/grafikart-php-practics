ece1a3ee583b3b626f9a9c8f26508e64cf96c539
Jean-Claude <jeanyao@ymail.com>
2023-11-30 00:02:05 +0300

    Upload des fichiers

--- php/www/views/post/show.php
+++ php/www/views/post/show.php
@@ -1,26 +1,27 @@
 <?php
+/** @var \App\Entity\Post $post */
 $parameter = new \Grafikart\Http\Bag\ParameterBag($params);
 $id   = $parameter->getInt('id');
 $slug = $parameter->get('slug');
 
 $connection = \App\Helpers\Connection::make();
 $postRepository = new \App\Repository\PostRepository($connection);
-$item = $postRepository->find($id);
+$post = $postRepository->find($id);
 $categoryRepository = new \App\Repository\CategoryRepository($connection);
-$categoryRepository->hydratePosts([$item]);
+$categoryRepository->hydratePosts([$post]);
 
-if ($item->getSlug() !== $slug) {
-    $url = $router->url('post', ['slug' => $item->getSlug(), 'id' => $id]);
+if ($post->getSlug() !== $slug) {
+    $url = $router->url('post', ['slug' => $post->getSlug(), 'id' => $id]);
     http_response_code(301);
     header("Location: $url");
     exit();
 }
 ?>
 
-<h1><?= e($item->getName()) ?></h1>
-<p class="text-muted"><?= $item->getCreatedAt()->format('d F Y') ?></p>
+<h1><?= e($post->getName()) ?></h1>
+<p class="text-muted"><?= $post->getCreatedAt()->format('d F Y') ?></p>
 <?php
-foreach ($item->getCategories() as $k => $category):
+foreach ($post->getCategories() as $k => $category):
     if ($k > 0):
          echo ', ';
     endif;
@@ -30,9 +31,15 @@ foreach ($item->getCategories() as $k => $category):
 <?php endforeach; ?>
 
 <?php if ($post->getImage()): ?>
-    <img src="<?= $post->getImageUrl('large') ?>" class="card-img-top" alt="<?= $post->getName()?>">
+    <p>
+        <img src="<?= $post->getImageUrl('large') ?>"
+             class="card-img-top"
+             alt="<?= $post->getName()?>"
+             style="width: 100%;"
+        >
+    </p>
 <?php endif; ?>
 
-<p><?= $item->getFormattedContent() ?></p>
+<p><?= $post->getFormattedContent() ?></p>
 
 
