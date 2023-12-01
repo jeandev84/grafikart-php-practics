223a6f8173d8ffbec8e75c1f5812809a5928db82
Jean-Claude <jeanyao@ymail.com>
2023-12-01 12:15:41 +0300

    Autoloading

---
+++ oop/01-fundamentals/06-autoloading/classes/Autoloader.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+
+/**
+ * Created by PhpStorm at 01.12.2023
+ *
+ * @Autoloader
+ *
+ * @author Jean-Claude <jeanyao@ymail.com>
+ *
+ * @package ${NAMESPACE}
+ */
+class Autoloader
+{
+     public static function register(): void
+     {
+         spl_autoload_register([__CLASS__, 'loadClass']);
+     }
+
+
+     protected function loadClass(string $class): void
+     {
+         require_once __DIR__."/classes/$class.php";
+     }
+}
