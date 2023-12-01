73ca0273d80a58f0179009673c09a46018682b20
Jean-Claude <jeanyao@ymail.com>
2023-12-02 02:52:16 +0300

    Connection to DB

--- oop/www/migrations/schema/data.sql
+++ oop/www/migrations/schema/data.sql
@@ -1,11 +1,19 @@
 -- Table : posts
-DROP TABLE posts;
-CREATE TABLE posts (
+DROP TABLE IF EXISTS posts;
+CREATE TABLE IF NOT EXISTS posts (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
     content LONGTEXT DEFAULT NULL,
     created_at DATETIME default CURRENT_TIMESTAMP,
+    category_id INTEGER DEFAULT NULL,
+    CONSTRAINT fk_categories FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE ON UPDATE RESTRICT,
     PRIMARY KEY (id)
 );
 
 INSERT INTO posts (title) VALUES ('Mon titre'), ('Mon titre'), ('Mon titre');
+
+DROP TABLE IF EXISTS categories;
+CREATE TABLE IF NOT EXISTS categories (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(255) DEFAULT NULL
+);
