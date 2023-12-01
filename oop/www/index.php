c6b0984c42dd03ba65ab2dcc7bd6852544a676bf
Jean-Claude <jeanyao@ymail.com>
2023-12-01 11:26:48 +0300

    Methods static

--- oop/www/index.php
+++ oop/www/index.php
@@ -1,12 +1,23 @@
 <?php
 
 require 'Form.php';
+require 'Person.php';
+require 'Text.php';
+
+
+$merlin = new Person('Merlin');
+$merlin->regenerate();
+var_dump($merlin);
 
 $form = new Form($_POST);
+
+var_dump(Text::withZero(10));
 ?>
 
 <form action="" method="POST">
-    <?= $form->input('username', 'Username') ?>
-    <?= $form->input('password', 'Password'); ?>
-    <?= $form->submit(); ?>
+    <?php
+      echo $form->input('username', 'Username');
+      echo $form->input('password', 'Username');
+      echo $form->submit();
+    ?>
 </form>
