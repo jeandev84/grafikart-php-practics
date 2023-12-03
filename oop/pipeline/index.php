8873ae6226e953c6784d9a34dc6c2481469c33cd
Jean-Claude <jeanyao@ymail.com>
2023-12-03 23:32:18 +0300

    Middleware principes

---
+++ oop/pipeline/index.php
@@ -0,0 +1,58 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use function Http\Response\send;
+
+require 'vendor/autoload.php';
+
+$trailingSlash = function (ServerRequestInterface $request, ResponseInterface $response, callable $next) {
+
+    $url = (string)$request->getUri();
+
+    if (!empty($url) && $url[-1] === '/') {
+       $response = new \GuzzleHttp\Psr7\Response();
+       return $response->withHeader('Location', substr($url, 0, -1))
+                        ->withStatus(301);
+   }
+
+   return $next($request, $response);
+};
+
+
+$quoteMiddleware = function (ServerRequestInterface $request, ResponseInterface $response, callable $next) {
+    $response->getBody()->write('"');
+    $response = $next($request, $response);
+    $response->getBody()->write('"');
+    return $response;
+};
+
+$app = function (ServerRequestInterface $request, ResponseInterface $response, callable $next) {
+
+    $url = $request->getUri()->getPath();
+
+    if ($url === '/blog') {
+        $response->getBody()->write('Je suis sur le blog');
+    } elseif ($url === '/contact') {
+        $response->getBody()->write('Me contacter');
+    } else {
+        $response->getBody()->write('Not found');
+        $response = $response->withStatus(404);
+    }
+
+    return $response;
+};
+
+$request = \GuzzleHttp\Psr7\ServerRequest::fromGlobals();
+$response = new \GuzzleHttp\Psr7\Response();
+
+$response = $trailingSlash($request, $response, function ($request, $response) use ($quoteMiddleware, $app) {
+    return $quoteMiddleware($request, $response, function ($request, $response) use ($app) {
+        return $app($request, $response, function ($request, $response) {
+             return $response;
+        });
+    });
+});
+
+
+send($response);
