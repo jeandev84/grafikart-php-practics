6ea4f56f6955e023c87da362e44497810123602e
Jean-Claude <jeanyao@ymail.com>
2023-12-06 01:42:56 +0300

    Simplifier formulaire

--- 03-oop-practics/www/src/Blog/Actions/AdminBlogAction.php
+++ 03-oop-practics/www/src/Blog/Actions/AdminBlogAction.php
@@ -6,6 +6,7 @@ namespace App\Blog\Actions;
 
 
 
+use App\Blog\Entity\Post;
 use App\Blog\Repository\PostRepository;
 use Framework\Actions\RouterAwareAction;
 use Framework\Routing\Router;
@@ -121,6 +122,7 @@ class AdminBlogAction
     public function create(Request $request): mixed
     {
         $errors = [];
+        $item = [];
 
         if ($request->getMethod() === 'POST') {
             $params = $this->getParams($request);
@@ -135,6 +137,8 @@ class AdminBlogAction
             $errors = $validator->getErrors();
         }
 
+        $item = new Post();
+        $item->created_at = new \DateTime();
         return $this->renderer->render("@blog/admin/create", compact('item', 'errors'));
     }
 
@@ -170,10 +174,11 @@ class AdminBlogAction
     private function getValidator(Request $request): Validator
     {
          return (new Validator($request->getParsedBody()))
-                ->required('content', 'name', 'slug')
+                ->required('content', 'name', 'slug', 'created_at')
                 ->length('content', 10)
                 ->length('name', 2, 250)
                 ->length('slug', 2, 50)
+                ->dateTime('created_at')
                 ->slug('slug');
     }
 }
