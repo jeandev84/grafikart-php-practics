147dc8ec2ab7079eb0e3f98128801c91938968b9
Jean-Claude <jeanyao@ymail.com>
2023-12-07 04:11:34 +0300

    Creation du query buildeur

--- 03-oop-practics/www/tests/Framework/Database/QueryTest.php
+++ 03-oop-practics/www/tests/Framework/Database/QueryTest.php
@@ -85,8 +85,6 @@ class QueryTest extends DatabaseTestCase
                  ->all()
         ;
 
-        var_dump($posts);
-        die;
         $this->assertEquals('demo', substr($posts[0]->getSlug(), -4));
     }
 
