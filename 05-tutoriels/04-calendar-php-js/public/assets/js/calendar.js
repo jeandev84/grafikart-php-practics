f67088a2c7b506a0c409663605b6d67eebe82b80
Jean Claude <jeanyao@ymail.com>
2024-01-16 12:44:51 +0500

    JS

--- 05-tutoriels/04-calendar-php-js/public/assets/js/calendar.js
+++ 05-tutoriels/04-calendar-php-js/public/assets/js/calendar.js
@@ -4,7 +4,15 @@ jQuery(function ($) {
 
     // show first month
     $('.month:first').show()
+
+    // activate current month
     $('.months-navbar a:first').addClass('color-active')
 
+    var current = 1;
 
+    $('.months-navbar a').click(function () {
+          var month = $(this).attr('id').replace('month-link-', '');
+          alert(month);
+          return false;
+    });
 })
